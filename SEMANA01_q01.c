5d6641b3ced73b475e8bfa5409b2146a6e7167ad
amanda batista <amandavieira23@acad.charqueadas.ifsul.edu.br>
2021-03-02 18:10:58 +0000

    add SEMANA01_q02.c

--- SEMANA01_q01.c
+++ SEMANA01_q01.c
@@ -1,15 +1,15 @@
-#include <stdio.h>
+// #include <stdio.h>
 
-int main(void) {
-  float valor;
+// int main(void) {
+//   float valor;
 
-  printf("Insira o valor dos itens comprados\n");
-  scanf("%f",&valor);
-  if(valor > 100 && valor <=200){
-    valor = valor - (valor*0.05);
-  }else if(valor > 200){
-    valor = valor - (valor*0.1);
-  }
-  printf("R$ %.2f\n",valor);
-  return 0;
-}
+//   printf("Insira o valor dos itens comprados\n");
+//   scanf("%f",&valor);
+//   if(valor > 100 && valor <=200){
+//     valor = valor - (valor*0.05);
+//   }else if(valor > 200){
+//     valor = valor - (valor*0.1);
+//   }
+//   printf("R$ %.2f\n",valor);
+//   return 0;
+// }
---
+++ SEMANA01_q02.c
@@ -0,0 +1,14 @@
+// #include <stdio.h>
+
+// int main(void) {
+//   int valor1, valor2, a1, a2, s1, s2;
+
+//   printf("Insira dois inteiros\n");
+//   scanf("%i %i",&valor1,&valor2);
+//   a1 = valor1 -1;
+//   s1 = valor1 +1;
+//   a2 = valor2 -1;
+//   s2 = valor2 +1;
+//   printf("%i %i %i %i %i %i\n",a1,valor1,s1,a2,valor2,s2);
+//   return 0;
+// }
