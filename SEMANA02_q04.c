d88d84fb788442ab59ee52222709c60be4821885
amanda batista <amandavieira23@acad.charqueadas.ifsul.edu.br>
2021-03-04 22:04:52 +0000

    SEMANA02_q04.c

---
+++ SEMANA02_q04.c
@@ -0,0 +1,31 @@
+// #include <stdio.h>
+
+// int main(void){
+//   int n1, n2,n3, nfigual;
+//   char nf;
+//   printf("Informe 3 notas de 1 a 4 \n");
+//   scanf("%i%i%i",&n1,&n2,&n3);
+  
+//   if((n1==4 && n2==4)||(n2==4 && n3==4)||(n1==4 && n3==4)){
+//     printf("REPROVADO\n");
+//   }else if((n1==n2) || (n2==n3) || (n1==n3)){
+//     if(n1==n2){
+//       nfigual = n1;
+//     }else if(n2==n3){
+//       nfigual = n2;
+//     }else if(n1==n3){
+//       nfigual = n1;
+//     }
+//     if(nfigual == 1){
+//       nf = 'A';
+//     }else if(nfigual == 2){
+//       nf = 'B';      
+//     }else if(nfigual == 3){
+//       nf = 'C';
+//     }
+//     printf("APROVADO COM %c\n",nf);
+//   }else{
+//     printf("INCONCLUSIVO\n");
+//   }
+//   return 0;
+// }
