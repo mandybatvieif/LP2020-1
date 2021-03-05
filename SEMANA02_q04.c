c3c030f2c073d241a1989acfea26ab78cde9b20c
amanda batista <amandavieira23@acad.charqueadas.ifsul.edu.br>
2021-03-05 23:10:33 +0000

    add validação para aceitar só val entre 1 e 4

--- SEMANA02_q04.c
+++ SEMANA02_q04.c
@@ -5,27 +5,30 @@
 //   char nf;
 //   printf("Informe 3 notas de 1 a 4 \n");
 //   scanf("%i%i%i",&n1,&n2,&n3);
-  
-//   if((n1==4 && n2==4)||(n2==4 && n3==4)||(n1==4 && n3==4)){
-//     printf("REPROVADO\n");
-//   }else if((n1==n2) || (n2==n3) || (n1==n3)){
-//     if(n1==n2){
-//       nfigual = n1;
-//     }else if(n2==n3){
-//       nfigual = n2;
-//     }else if(n1==n3){
-//       nfigual = n1;
+//   if((n1>=1 && n1 <=4) && (n2>=1 && n2 <=4) && (n3>=1 && n3 <=4)){
+//     if((n1==4 && n2==4)||(n2==4 && n3==4)||(n1==4 && n3==4)){
+//       printf("REPROVADO\n");
+//     }else if((n1==n2) || (n2==n3) || (n1==n3)){
+//       if(n1==n2){
+//         nfigual = n1;
+//       }else if(n2==n3){
+//         nfigual = n2;
+//       }else if(n1==n3){
+//         nfigual = n1;
+//       }
+//       if(nfigual == 1){
+//         nf = 'A';
+//       }else if(nfigual == 2){
+//         nf = 'B';      
+//       }else if(nfigual == 3){
+//         nf = 'C';
+//       }
+//       printf("APROVADO COM %c\n",nf);
+//     }else{
+//       printf("INCONCLUSIVO\n");
 //     }
-//     if(nfigual == 1){
-//       nf = 'A';
-//     }else if(nfigual == 2){
-//       nf = 'B';      
-//     }else if(nfigual == 3){
-//       nf = 'C';
-//     }
-//     printf("APROVADO COM %c\n",nf);
 //   }else{
-//     printf("INCONCLUSIVO\n");
+//     printf("Inserir apenas valores entre 1 e 4 (incluindo 1 e 4)\n");
 //   }
 //   return 0;
 // }
