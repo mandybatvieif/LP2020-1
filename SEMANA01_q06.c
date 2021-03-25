49dc5d34303a1ddd1839c4b51bcf65076f8c0f3b
amanda batista <amandavieira23@acad.charqueadas.ifsul.edu.br>
2021-03-25 02:40:42 +0000

    add SEMANA01_q06.c

---
+++ SEMANA01_q06.c
@@ -0,0 +1,33 @@
+// #include <stdio.h>
+
+// int main(void){
+//   int a;
+//   printf("Informe 1 número inteiro de 0 a 9\n");
+//   scanf("%i",&a);
+//   if(a >= 0 && a <= 9){
+//     if(a == 0){
+//       printf("zero\n");
+//     }else if(a == 1){
+//       printf("one\n");
+//     }else if(a == 2){
+//       printf("two\n");
+//     }else if(a == 3){
+//       printf("three\n");
+//     }else if(a == 4){
+//       printf("four\n");
+//     }else if(a == 5){
+//       printf("five\n");
+//     }else if(a == 6){
+//       printf("six\n");
+//     }else if(a == 7){
+//       printf("seven\n");
+//     }else if(a == 8){
+//       printf("eight\n");
+//     }else if(a == 9){
+//       printf("nine\n");
+//     }
+//   }else{
+//     printf("valor não permitido\n");
+//   }
+//   return 0;
+// }
