32382323f0a151a2e1843a3377dbde32979fdb78
amanda batista <amandavieira23@acad.charqueadas.ifsul.edu.br>
2021-03-25 03:16:37 +0000

    SEMANA01_q09.c

---
+++ SEMANA01_q09.c
@@ -0,0 +1,50 @@
+// #include <stdio.h>
+
+// int main(void){
+//   int a, n100, n50, n20, n10, n5, n2, n1;
+//   printf("Informe 1 número inteiro\n");
+//   scanf("%i",&a);
+//   n100 = a/100;
+//   a= a%100;
+
+//   n50 = a/50;
+//   a= a%50;
+
+//   n20 = a/20;
+//   a= a%20;
+
+//   n10 = a/10;
+//   a= a%10;
+
+//   n5 = a/5;
+//   a= a%5;
+
+//   n2 = a/2;
+//   a= a%2;
+
+//   n1 = a/1;
+//   a= a%1;
+//   if(n100 > 0){
+//     printf("%i notas de R$ 100\n",n100);
+//   }
+//   if(n50 > 0){
+//   printf("%i notas de R$ 50\n",n50);
+//   }
+//   if(n20> 0){
+//     printf("%i notas de R$ 20\n",n20);
+//   }
+//   if(n10 > 0){
+//     printf("%i notas de R$ 10\n",n10);
+//   }
+//   if(n5 > 0){
+//     printf("%i notas de R$ 5\n",n5);
+//   }
+//   if(n2 > 0){
+//      printf("%i notas de R$ 2\n",n2); 
+//   }
+//   if(n1 > 0){
+//     printf("%i notas de R$ 1\n",n1);
+//   }
+
+//   return 0;
+// }
