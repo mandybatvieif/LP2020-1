521249c6c1827d6d3443294a7c385e1399951f56
amanda batista <>
2021-04-08 01:00:54 +0000

    add SEMANA01_q08.c

---
+++ SEMANA01_q08.c
@@ -0,0 +1,16 @@
+// #include <stdio.h>
+// int main(void) {
+//   float x, y;
+
+//   scanf("%f", &x);
+//   scanf("%f", &y);
+
+//   if((y >= 204 && y <= 244) && (x >= 50 || x <= 58 || x >= 750 || x <= 790)){
+//     printf("GOLACO\n");
+//   }else if ((y <= 244) &&(x >= 58 && x <= 790)){
+//     printf("GOL\n");
+//   }else if((x >= 50 && x <= 58) || (x >= 790 && x <= 798) || (y >= 244 && y <= 254) || (x < 50 || x > 798 || y > 254)){
+//     printf("UHHH\n");
+//   } 
+//   return 0;
+// }
