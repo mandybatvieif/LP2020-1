f429ab7eb6cf58e0fa223e1c7d0b63984f0b171d
amanda batista <>
2021-04-08 00:48:15 +0000

    add SEMANA01_q11.c

---
+++ SEMANA01_q11.c
@@ -0,0 +1,24 @@
+// #include <stdio.h>
+// int main () {
+//   int n1, n2;
+//   scanf("%i", &n1);
+//   int soma_p = 0, soma_i = 0;
+//   int i;
+//   for (i=1; i<=12; i++) {
+//     scanf("%i", &n2);
+//     if (i % 2 == 0) {
+//       soma_p += n2;
+//     }else{
+//       soma_i += n2;
+//     }
+    
+//   }
+//   int soma_final = soma_i*3 + soma_p;
+//   if ((soma_final + n1) % 10 == 0 ) {
+//     printf("S\n");
+//   }else{
+//     printf("N\n");
+//   }
+
+//   return 0;
+// }
